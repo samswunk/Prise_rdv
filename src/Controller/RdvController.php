832af89d882301bdbf9b375be79ba59ebb65d561
Samuel <lesourd.samuel@free.fr>
2020-06-10 17:04:53 +0200

    calendar bundle

---
+++ src/Controller/RdvController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RdvController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('base.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
+
+?>
---
+++ src/Entity/Booking.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookingRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BookingRepository::class)
+ */
+class Booking
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
