dc45d4f35927022f1974cba6ddd23835692eb393
Samuel <lesourd.samuel@free.fr>
2020-07-29 17:10:36 +0200

    install js

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -4,9 +4,11 @@ namespace App\Form;
 
 use App\Entity\Booking;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 
 class BookingType extends AbstractType
 {
@@ -14,9 +16,19 @@ class BookingType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('start',DateType::class,[
-                
-            ])
+            ->add('start', DateTimeType::class, array
+                    (
+                        'widget'=> 'single_text',
+                        'required' => true,
+                        'attr' => 
+                            [
+                                'class' => 'form-control input-inline datetimepicker',
+                                'data-provide' => 'datetimepicker'
+                            ],
+                        'html5'=> false, 
+                        'format'=> 'YYYY-MM-dd HH:mm'
+                    )
+                )
             ->add('end')
             ->add('description')
         ;
